73e3678d2063af1b032e25190e45025df67f60c2
BOAB235 <BOAB785@proton.me>
2026-02-17 11:08:57 +0100

    EMI test

--- removeCheckpointsFolder.py
+++ removeCheckpointsFolder.py
@@ -1,9 +1,25 @@
-import os
+_="""import os
 import shutil
 
 for root, dirs, files in os.walk(".", topdown=False):
     for d in dirs:
-        if d == ".ipynb_checkpoints":
+        if d == ".ipynb_checkpoints" or __pycache__:
+            folder_path = os.path.join(root, d)
+            print(f"Removing: {folder_path}")
+            shutil.rmtree(folder_path, ignore_errors=True)
+
+print("Cleanup complete!")
+
+"""
+
+import os
+import shutil
+
+TARGETS = {".ipynb_checkpoints", "__pycache__"}
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for d in dirs:
+        if d in TARGETS:
             folder_path = os.path.join(root, d)
             print(f"Removing: {folder_path}")
             shutil.rmtree(folder_path, ignore_errors=True)
